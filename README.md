63a28d421c2f7457b7492a54a446a5348b6faf6c
jmacura <macurajan@gmail.com>
2024-02-29 22:11:49 +0100

    Add control for empty GPSInfo in EXIF & add CLI help

--- README.md
+++ README.md
@@ -10,9 +10,13 @@ This tool does generally the same thing as https://github.com/briangkatz/gps-pho
 * ``` pip install Pillow geojson ```
 
 ## Usage
-The tool takes all the JPEG files in one folder and creates a GeoJSON containing FeatureCollection of Points, where each point represents the location of one photo. To simply identify the individual point, the image's file name is saved an "id" property of each point.
+The tool takes all the JPEG files in one folder and creates a GeoJSON containing FeatureCollection of Points, where each point represents the location of one photo. To simply identify the individual point, the image's file name is saved as the "id" property of each point.
 
 * ``` photos2geojs.py ```
 * the `photos.geojson` file will appear in your working directory
 
 Check the `photos.geojson` file in this repository to have an idea what it creates.
+
+You can use `--ignore-empty` and `--skip-empty` options to control the output for images without georeference.
+
+Help is available via `photos2geojs.py -h` as usual.
--- photos2geojs.py
+++ photos2geojs.py
@@ -1,6 +1,7 @@
 # @author: jmacura 2019
 # with a huge help of: https://developer.here.com/blog/getting-started-with-geocoding-exif-image-metadata-in-python3
 
+import argparse
 from geojson import Point, Feature, FeatureCollection
 from geojson import dumps as geodumps
 from glob import glob
@@ -49,6 +50,8 @@ def get_decimal_from_dms(dms, ref):
 
 # source: https://developer.here.com/blog/getting-started-with-geocoding-exif-image-metadata-in-python3
 def get_coordinates(geotags):
+    if not geotags:
+        raise ValueError("No EXIF geotagging found")
     lat = get_decimal_from_dms(geotags['GPSLatitude'], geotags['GPSLatitudeRef'])
     lon = get_decimal_from_dms(geotags['GPSLongitude'], geotags['GPSLongitudeRef'])
     return (lon, lat) #intentionally GeoJSON swapped order
@@ -57,12 +60,40 @@ def make_geojs_feature(coords, filename):
     prop = {"id": filename.split(".")[0]}
     return Feature(geometry = Point(coords), properties = prop)
 
+# === DEFINE CLI OPTIONS ===
+parser = argparse.ArgumentParser(
+    description='Convert georeferenced images into a collection of points in GeoJSON.',
+    epilog='MPL-2.0 license'
+)
+parser.add_argument('--ignore-empty', action='store_true',
+                    help='Ignore empty georeference. Instead of raising an error, it will create point at the Null island [0,0].')
+parser.add_argument('--skip-empty', action='store_true',
+                    help='Skip empty georeference. Instead of raising an error, no point for this image will be created. Takes precedence over --ignore-empty option.')
+
+#=== EXECUTE ===
+args = parser.parse_args()
+#pprint(args)
+
 features = []
 for filename in glob('*.jpg'):
     if not filename.endswith('(2).jpg'):
         exif = get_exif(filename)
-        geotags = get_geotagging(exif)
-        coords = get_coordinates(geotags)
+        try:
+            geotags = get_geotagging(exif)
+        except ValueError as e:
+            if args.skip_empty:
+                continue
+            elif args.ignore_empty:
+                geotags = None
+            else:
+                raise e
+        try:
+            coords = get_coordinates(geotags)
+        except ValueError as e:
+            if args.ignore_empty:
+                coords = (0, 0)
+            else:
+                raise e
         feature = make_geojs_feature(coords, filename)
         features.append(feature)
         #pprint(feature)
